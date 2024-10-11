b8d55befc8cc75d134a3b69525b592f2a5187aea
Johnny <jonathanbc2012@gmail.com>
2024-10-11 10:10:41 +0200

    feat: PermissionDTO and PermissionEntity added

--- crm/src/permission/model/PermissionDTO.java
+++ crm/src/permission/model/PermissionDTO.java
@@ -1,4 +1,32 @@
 package permission.model;
 
 public class PermissionDTO {
+    /*
+    - nombre (String): Nombre del permiso.
+     */
+
+    private String name;
+
+    public PermissionDTO(String name) {
+        this.name = name;
+    }
+
+    public PermissionDTO(){
+
+    }
+
+    public String getName(){
+        return  name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "PermissionDTO{" +
+                "name='" + name + '\'' +
+                '}';
+    }
 }
