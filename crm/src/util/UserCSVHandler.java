cd661ba836ea24dc0096c44e100a611753bd8bfd
Johnny <jonathanbc2012@gmail.com>
2024-10-11 11:28:16 +0200

    feat: userCSVHandler added

--- crm/src/util/UserCSVHandler.java
+++ crm/src/util/UserCSVHandler.java
@@ -1,5 +1,49 @@
 package util;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+
 public class UserCSVHandler {
+    public File ficheroPath = new File("");
+
+    private BufferedReader lector; //lee el archivo
+    private String linea; // recibe la línea de cada fila
+    private String partes[] = null; //almacena cada línea
+
+    public void writeNewUserLine(String id, String nombre, String email, int edad) {
+        try (FileWriter escribir = new FileWriter(ficheroPath, true)) { // true para agregar sin sobrescribir
+            escribir.write(  id + "," + nombre + "," + email + "," + edad + "\n");
+            escribir.close();
+        } catch (Exception e) {
+            System.out.println("Se ha producido un error al escribir en el archivo.");
+        }
+    }
+    public void escribirLineaNueva(){
+    }
+
+
+    public void leerArchivo(String nombreArchivo) {
+        try {
+            lector = new BufferedReader(new FileReader(nombreArchivo));
+            while ((linea = lector.readLine()) != null) {
+                partes = linea.split(",");
+                imprimirLinea();
+                System.out.println();
+            }
+            lector.close();
+            linea = null;
+            partes = null;
+        } catch (Exception e) {
+            System.out.println("Se ha producido un error");
+        }
+    }
+
+    public void imprimirLinea(){
+        for (int i = 0; i < partes.length; i++) {
+            System.out.print(partes[i] + "  |  ");
+        }
+    }
 
 }
